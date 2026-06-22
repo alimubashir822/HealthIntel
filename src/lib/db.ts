3e5520764fc259db381b118faa7c10960454ddae
Mubashir Ali <alimubashir822@gmail.com>
2026-06-23 01:40:54 +0500

    Configure database connection to use local SQLite database

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-function createPrismaClient() {
-  return new PrismaClient().$extends(withAccelerate());
-}
-
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
